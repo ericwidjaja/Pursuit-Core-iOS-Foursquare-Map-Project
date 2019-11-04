9ba89aad8af3167923277a25683770ae611c7760
Eric Widjaja <ericwidjaja@pursuit.org>
2019-11-04 12:25:56 -0500

    Initial Commit

---
+++ Foursquare-Map-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foursquare-Map-Project
+//
+//  Created by Eric Widjaja on 11/4/19.
+//  Copyright © 2019 Eric W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Foursquare-Map-ProjectTests/Foursquare_Map_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Foursquare_Map_ProjectTests.swift
+//  Foursquare-Map-ProjectTests
+//
+//  Created by Eric Widjaja on 11/4/19.
+//  Copyright © 2019 Eric W. All rights reserved.
+//
+
+import XCTest
+@testable import Foursquare_Map_Project
+
+class Foursquare_Map_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
