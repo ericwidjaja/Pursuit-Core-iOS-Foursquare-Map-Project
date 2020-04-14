42c00c1e8e99385cc790c8e127289c436d8d1a25
Eric Widjaja <ericwidjaja@pursuit.org>
2020-04-13 21:01:51 -0400

    change Secret File

--- Foursquare-Map-Project/Secret.swift
+++ Foursquare-Map-Project/Secret.swift
@@ -1,12 +1,11 @@
-
 //  Secret.swift
 //  Foursquare-Map-Project
-//
 //  Created by Eric Widjaja on 11/6/19.
 //  Copyright © 2019 Eric W. All rights reserved.
-//
+
 
 import Foundation
+
 
 class Secrets {
     static let myID = "PO30AILO2IJRCZEI4MI5RPYZ1PB5T5CSRV4DCTEJVH0ECEPL"
